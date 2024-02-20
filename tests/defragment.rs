8bfd04b8dc49b59d2e40de34fa92f8194536e1b8
Jim Kring <jim.kring@jki.net>
2024-02-20 09:23:17 -0800

    added copy_channel method

--- tests/defragment.rs
+++ tests/defragment.rs
@@ -1,5 +1,62 @@
 mod common;
-use tedium::{ChannelPath, PropertyPath, PropertyValue, TdmsFile};
+use tedium::{ChannelPath, PropertyPath, PropertyValue, TdmsFile, TdmsStorageType};
+
+
+// Comment from the tedium author:
+//
+// So it should just adapt to the type of your array. There is a trait TdmsStorageType which is implemented for supported types for channels. So if you just use an array of f64 for example it will adapt to that.
+//
+// Now I can imagine how that could go wrong with defrag, if read is generic, and write is generic - it has to resolve to a type somewhere!
+// You almost want a copy_channel method which is generic and then above that check the type and call the right version.
+//
+// fn copy_channel_data<D: TdmsStorageType>(source, dest, channel) {
+//     generic_read(),
+//     generic_write()
+// }
+//
+// fn copy_channel(source, dest, channel) {
+//     let type = source.get_channel_type();
+//     match type {
+//         f64 => copy_channel_data::<f64>(source, dest, channel)
+//     }
+// }
+
+// let's implement the copy_channel_data method
+fn copy_channel_data<D: TdmsStorageType>(source: &mut TdmsFile<F>, dest: &mut TdmsFile<F>, channel: &ChannelPath) {
+    let channel_length = source.channel_length(channel).unwrap();
+    let mut data = vec![0.0; channel_length as usize];
+    source.read_channel(channel, &mut data).unwrap();
+    dest.write_channels(&[channel.clone()], &data, tedium::DataLayout::Interleaved).unwrap();
+}
+
+// let's implement the copy_channel method
+fn copy_channel(source: &mut TdmsFile<F>, dest: &mut TdmsFile<F>, channel: &ChannelPath) {
+    let channel_type = source.channel_type(channel).unwrap();
+    match channel_type {
+        TdmsStorageType::Double => copy_channel_data::<f64>(source, dest, channel),
+        TdmsStorageType::Single => copy_channel_data::<f32>(source, dest, channel),
+        TdmsStorageType::Int64 => copy_channel_data::<i64>(source, dest, channel),
+        TdmsStorageType::Int32 => copy_channel_data::<i32>(source, dest, channel),
+        TdmsStorageType::Int16 => copy_channel_data::<i16>(source, dest, channel),
+        TdmsStorageType::Int8 => copy_channel_data::<i8>(source, dest, channel),
+        TdmsStorageType::UInt64 => copy_channel_data::<u64>(source, dest, channel),
+        TdmsStorageType::UInt32 => copy_channel_data::<u32>(source, dest, channel),
+        TdmsStorageType::UInt16 => copy_channel_data::<u16>(source, dest, channel),
+        TdmsStorageType::UInt8 => copy_channel_data::<u8>(source, dest, channel),
+        TdmsStorageType::String => {
+            let channel_length = source.channel_length(channel).unwrap();
+            let mut data = vec![String::new(); channel_length as usize];
+            source.read_channel(channel, &mut data).unwrap();
+            dest.write_channels(&[channel.clone()], &data, tedium::DataLayout::Interleaved).unwrap();
+        }
+        TdmsStorageType::Boolean => {
+            let channel_length = source.channel_length(channel).unwrap();
+            let mut data = vec![false; channel_length as usize];
+            source.read_channel(channel, &mut data).unwrap();
+            dest.write_channels(&[channel.clone()], &data, tedium::DataLayout::Interleaved).unwrap();
+        }
+    }
+}
 
 
 #[test]
@@ -62,8 +119,11 @@ fn test_defragment() {
             // print the data
             println!("data: {:?}", data);
 
-            // Write the channel data to the output file
-            writer.write_channels(&[channel_path], &data, tedium::DataLayout::Interleaved).unwrap();
+            // // Write the channel data to the output file
+            // writer.write_channels(&[channel_path], &data, tedium::DataLayout::Interleaved).unwrap();
+
+            // copy the channel data to the output file
+            copy_channel(&mut input_file, &mut output_file, &channel_path);
         }
     }
 
